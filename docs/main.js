8066daec2534f4bbec9afe8c979ef2520ac90f70
Dele Olajide <dele@4ng.net>
2021-04-20 10:19:56 +0100

    support multiple profiles

--- docs/main.js
+++ docs/main.js
@@ -13969,17 +13969,20 @@ async function getToken(creds)
   {
 	const client = new CommunicationIdentityClient(config.acs_endpoint);
 	const scopes = ["voip"];
+	
+	BrowserDetect.init();
+	const profile = "acs_profile_" + (BrowserDetect.browser + BrowserDetect.version + BrowserDetect.OS).toLowerCase();	
 
-	if (!config.acs_user_endpoint)
+	if (!config[profile])
 	{
 		const user = await client.createUser();	
 		console.log("Created user endpoint", user);		
-		config.acs_user_endpoint = user.communicationUserId;	
-		const options = {method: "POST", headers: {authorization}, body: config.acs_user_endpoint };			
-		const response = await fetch(url + "/acs_user_endpoint", options);
+		config[profile] = user.communicationUserId;	
+		const options = {method: "POST", headers: {authorization}, body: config[profile] };			
+		const response = await fetch(url + "/" + profile, options);
 	}
 
-	return client.getToken({communicationUserId: config.acs_user_endpoint}, scopes);
+	return client.getToken({communicationUserId: config[profile]}, scopes);
   }	
 }
 
@@ -14005,6 +14008,154 @@ function bufferEncode(e)
 	for (const e of t) n += String.fromCharCode(e);
 	return btoa(n).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "")
 }
+
+const BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "unknown";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "unknown";
+		this.OS = this.searchString(this.dataOS) || "unknown";
+
+		this.width = 0;
+		this.height = 0;
+
+		if ( typeof( window.innerWidth ) == 'number' )
+		{
+			this.width = window.innerWidth;
+			this.height = window.innerHeight;
+
+		} else if ( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+
+			this.width = document.documentElement.clientWidth;
+			this.height = document.documentElement.clientHeight;
+
+		} else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+
+			this.width = document.body.clientWidth;
+			this.height = document.body.clientHeight;
+		}
+	},
+
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++) {
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+
+	dataBrowser: [
+		{
+			string: navigator.userAgent,
+			subString: "Chrome",
+			identity: "Chrome"
+		},
+		{   string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari",
+			versionSearch: "Version"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{       // for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{       // for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+
+	dataOS : [
+		{
+			string: navigator.userAgent,
+			subString: "Windows NT 10.0; Win64",
+			identity: "Win10.64"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Windows NT 10.0",
+			identity: "Win10"
+		},
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Win"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+		   string: navigator.userAgent,
+		   subString: "iPhone",
+		   identity: "iPhone"
+		},
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+};
 })();
 
 /******/ })()
