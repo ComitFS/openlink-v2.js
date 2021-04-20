bf1b0c51e0ad434fdb067ecd51693ee1bf524df7
Dele Olajide <dele@4ng.net>
2021-04-20 16:45:48 +0100

    add build of acs module

---
+++ acs/src/index.js
@@ -0,0 +1,3 @@
+export const { CommunicationIdentityClient } = require("@azure/communication-identity");
+export const { CallClient, CallAgent, Renderer, LocalVideoStream} = require("@azure/communication-calling");
+export const { AzureCommunicationTokenCredential }  = require("@azure/communication-common");
