e3a275a8778834f9f7c0793a7bfb1aded8407036
Dele Olajide <dele@4ng.net>
2021-10-15 12:30:17 +0100

    upgrade code to bleeding edge

--- acs/src/index.js
+++ acs/src/index.js
@@ -1,5 +1,6 @@
 import { CommunicationIdentityClient } from "@azure/communication-identity";
 import { CallClient, CallAgent, Renderer, LocalVideoStream} from "@azure/communication-calling";
 import { AzureCommunicationTokenCredential } from "@azure/communication-common";
+import { PublicClientApplication, LogLevel, InteractionRequiredAuthError } from "@azure/msal-browser";
 
-window.ACS =  {CommunicationIdentityClient, CallClient, CallAgent, AzureCommunicationTokenCredential};
+window.ACS =  {CommunicationIdentityClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError};
