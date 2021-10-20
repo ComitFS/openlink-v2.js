d4f94fd60aafd410615156c9b9991d34015a33c0
Dele Olajide <dele@4ng.net>
2021-10-20 19:12:03 +0100

    added Features for transfer call

--- acs/src/index.js
+++ acs/src/index.js
@@ -1,6 +1,6 @@
 import { CommunicationIdentityClient } from "@azure/communication-identity";
-import { CallClient, CallAgent, Renderer, LocalVideoStream} from "@azure/communication-calling";
+import { CallClient, CallAgent, Renderer, LocalVideoStream, Features} from "@azure/communication-calling";
 import { AzureCommunicationTokenCredential } from "@azure/communication-common";
 import { PublicClientApplication, LogLevel, InteractionRequiredAuthError } from "@azure/msal-browser";
 
-window.ACS =  {CommunicationIdentityClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError};
+window.ACS =  {CommunicationIdentityClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError, Features};
