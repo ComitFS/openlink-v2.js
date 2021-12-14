e60f9acb9020723ee3ffe16d47deea60ee40d65d
Dele Olajide <dele@4ng.net>
2021-12-14 18:12:22 +0000

    upgrade to latest acs code

--- acs/src/index.js
+++ acs/src/index.js
@@ -1,6 +1,7 @@
 import { CommunicationIdentityClient } from "@azure/communication-identity";
 import { CallClient, CallAgent, Renderer, LocalVideoStream, Features} from "@azure/communication-calling";
+import { ChatClient } from '@azure/communication-chat';
 import { AzureCommunicationTokenCredential } from "@azure/communication-common";
 import { PublicClientApplication, LogLevel, InteractionRequiredAuthError } from "@azure/msal-browser";
 
-window.ACS =  {CommunicationIdentityClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError, Features};
+window.ACS =  {CommunicationIdentityClient, ChatClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError, Features};
