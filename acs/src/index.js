d7695551f4d52ed9eb57032653b4c3620dd4ca67
Dele Olajide <dele@4ng.net>
2022-01-19 19:27:34 +0000

    Expose Renderer

--- acs/src/index.js
+++ acs/src/index.js
@@ -1,7 +1,17 @@
 import { CommunicationIdentityClient } from "@azure/communication-identity";
-import { CallClient, CallAgent, Renderer, LocalVideoStream, Features} from "@azure/communication-calling";
+import { CallClient, LocalVideoStream, Features, VideoStreamRenderer} from "@azure/communication-calling";
 import { ChatClient } from '@azure/communication-chat';
 import { AzureCommunicationTokenCredential } from "@azure/communication-common";
-import { PublicClientApplication, LogLevel, InteractionRequiredAuthError } from "@azure/msal-browser";
+import { PublicClientApplication, LogLevel, InteractionRequiredAuthError} from "@azure/msal-browser";
 
-window.ACS =  {CommunicationIdentityClient, ChatClient, CallClient, CallAgent, AzureCommunicationTokenCredential, PublicClientApplication, LogLevel, InteractionRequiredAuthError, Features};
+window.ACS =  { CommunicationIdentityClient, 
+				ChatClient, 
+				CallClient, 
+				AzureCommunicationTokenCredential, 
+				PublicClientApplication, 
+				LogLevel, 
+				InteractionRequiredAuthError, 
+				LocalVideoStream, 
+				Features,
+				VideoStreamRenderer
+};
