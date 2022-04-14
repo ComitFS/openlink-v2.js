fd1ff9d8fb36cb62f703bd43502f97963f833114
Dele Olajide <dele@4ng.net>
2022-04-14 15:44:33 +0100

    add smsClient

--- acs/src/index.js
+++ acs/src/index.js
@@ -1,13 +1,15 @@
 import { CommunicationIdentityClient } from "@azure/communication-identity";
 import { CallClient, LocalVideoStream, Features, VideoStreamRenderer} from "@azure/communication-calling";
 import { ChatClient } from '@azure/communication-chat';
+import { SmsClient } from "@azure/communication-sms";
 import { AzureCommunicationTokenCredential } from "@azure/communication-common";
-import { PublicClientApplication, LogLevel, InteractionRequiredAuthError} from "@azure/msal-browser";
-import {LitElement, html, css} from 'lit';
+import { PublicClientApplication, LogLevel, InteractionRequiredAuthError } from "@azure/msal-browser";
+import { LitElement, html, css } from 'lit';
 
 window.ACS =  { CommunicationIdentityClient, 
 				ChatClient, 
 				CallClient, 
+				SmsClient,
 				AzureCommunicationTokenCredential, 
 				PublicClientApplication, 
 				LogLevel, 
